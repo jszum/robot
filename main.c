0e3c29106c3e29890ee99907a0b6151b41c836b2
jszumega <jaroslaw.szumega@gmail.com>
2018-05-27 02:36:12 +0200

    Map: examples of usage.

--- main.c
+++ main.c
@@ -5,19 +5,34 @@
 
 int main(int argc, char *argv[])
 {
+    setbuf(stdout, NULL);
     int* map = NULL;
-    int rows = 5;
-    int columns = 3;
+    int rows = 11;
+    int columns = 11;
 
 
     int mapsize = calcAdjustedSize(rows, columns);
-    map = (int*)calloc(mapsize, sizeof(int));
+    //map = (int*)calloc(mapsize, sizeof(int));
+    map = allocateMap(rows, columns);
 
     printf("C-required size = %d\n", rows*columns);
     printf("Opt required size = %d\n", mapsize);
 
+    int sign = getCell(map,rows,columns,4,2);
+    moveForward(map, rows, columns);
+    moveForward(map, rows, columns);
+    moveForward(map, rows, columns);
+    setNextCellFree(map, rows, columns);
+    turnLeft();
+    moveForward(map, rows, columns);
+    moveForward(map, rows, columns);
+    setCellBlockedAtDistance(map, rows, columns, 2);
+    turnRight();
+    setCellAheadBlocked(map, rows, columns);
+    turnBack();
+    setNCellsAheadFree(map,rows,columns,5);
+    int status = isNextCellFree(map, rows, columns);
 
-    int value = getCell(map,rows,columns,2,1);
 
     print_map(map,rows,columns);
     return 0;
